acb05e516df21cae36e77d2db9105bbc774b7e01
Siddarth Dagar <sidmcheath@gmail.com>
2020-12-12 17:54:14 -0500

    Implemented working VADER analysis

--- vader.py
+++ vader.py
@@ -3,6 +3,17 @@ University of Toronto CSC110 Final Project: Sentiment Analysis of Climate Change
 Siddarth Dagar, Bradley Mathi, Backer Jackson, Daniel Zhu
 """
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('vader_lexicon')
 
 test_set = ["An eye-opening article. This further reinforces the need to switch to a more enviroment \
             friendly lifestyle. @EamonRyan thank you for sharing this!",
@@ -12,14 +23,20 @@ test_set = ["An eye-opening article. This further reinforces the need to switch 
             concert with cynical, deceitful and greedy fossil fuel lobbyists. They fiddle; our kids burn."]
 
 
-# idk what i want this to return yet
 def main(sentences: list) -> list:
+    """
+    Execute VADER analysis. Return a list of tuples, where (negative, neutral, positive, compound)
+     are the scores given. If this were to be run on test_set, the result should be
+     [[0.0, 0.665, 0.335, 0.843], [0.192, 0.808, 0.0, -0.2263], [0.329, 0.671, 0.0, -0.9136]]
+    :param sentences:
+    :return:
+    """
     darth = SentimentIntensityAnalyzer()
     collector = []
     for sentence in sentences:
-        print(sentence)
         ss = darth.polarity_scores(sentence)
-        for k in sorted(ss):
-            print('{0}: {1}, '.format(k, ss[k]), end='')
-        print()
+        temp = []
+        for k in ss.values():
+            temp.append(k)
+        collector.append(temp)
     return collector
