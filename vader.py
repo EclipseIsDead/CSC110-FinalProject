9f07c7700e10171b7f98ae0c2daf7c59eaaca9c4
Siddarth Dagar <sidmcheath@gmail.com>
2020-12-12 17:36:28 -0500

    SSL Certificate Workaround and VADER Test

--- vader.py
+++ vader.py
@@ -3,19 +3,7 @@ University of Toronto CSC110 Final Project: Sentiment Analysis of Climate Change
 Siddarth Dagar, Bradley Mathi, Backer Jackson, Daniel Zhu
 """
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import nltk
-import ssl
 
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-nltk.download('vader_lexicon')
-
-# tweet id's :
 test_set = ["An eye-opening article. This further reinforces the need to switch to a more enviroment \
             friendly lifestyle. @EamonRyan thank you for sharing this!",
             "Bangladesh Confronts Climate Change - book goes open access http://upflow.co/l/Rc88",
@@ -24,17 +12,14 @@ test_set = ["An eye-opening article. This further reinforces the need to switch 
             concert with cynical, deceitful and greedy fossil fuel lobbyists. They fiddle; our kids burn."]
 
 
-def vader_test(sentences: list) -> None:
-    """
-    This should print sentiment analysis for each sentence in sentences.
-    *elaborate*
-    :param sentences:
-    :return:
-    """
+# idk what i want this to return yet
+def main(sentences: list) -> list:
     darth = SentimentIntensityAnalyzer()
+    collector = []
     for sentence in sentences:
         print(sentence)
         ss = darth.polarity_scores(sentence)
         for k in sorted(ss):
             print('{0}: {1}, '.format(k, ss[k]), end='')
         print()
+    return collector
