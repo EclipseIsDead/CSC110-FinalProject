baf27126ade33263df9152fecd20e3363102274e
Siddarth Dagar <sidmcheath@gmail.com>
2020-12-13 13:08:24 -0500

    Corrected plotting, added to main

--- plotting.py
+++ plotting.py
@@ -6,18 +6,21 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
-test = pd.DataFrame([[5.1, 3.5, 0], [4.9, 3.0, 0], [7.0, 3.2, 1],
-                     [6.4, 3.2, 1], [5.9, 3.0, 2]],
+test = pd.DataFrame([[5.1, 3.5, 0],
+                     [4.9, 3.0, 0],
+                     [7.0, 3.2, 1],
+                     [6.4, 3.2, 1],
+                     [5.9, 3.0, 2]],
                     columns=['length', 'width', 'species'])
 
 
-def plot_scatter(x: list, y: list) -> None:
-    """Kill me"""
+def plot_list(x: list, y: list) -> None:
+    """Lmao"""
     fig = px.scatter(x, y)
     fig.show()
 
 
-def plot_df(data: pd.DataFrame, xax: str, yax: str) -> None:
+def plot_scatter_df(data: pd.DataFrame, xax: str, yax: str) -> None:
     """Kill me"""
     fig = px.scatter(data, x=xax, y=yax)
     fig.show()
