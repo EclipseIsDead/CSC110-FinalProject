43ff42bf4bb7daebbac958ca106cbe8ee3622e9a
Siddarth Dagar <sidmcheath@gmail.com>
2020-12-13 14:53:28 -0500

    Added code for initial tweet analysis

---
+++ initial_analysis.py
@@ -0,0 +1,24 @@
+"""
+University of Toronto CSC110 Final Project: Sentiment Analysis of Climate Change Tweets
+Siddarth Dagar, Bradley Mathi, Backer Jackson, Daniel Zhu
+"""
+import pandas as pd
+import vader
+import plotting
+import plotly.express as px
+
+data = pd.read_csv('datasets/twitter_sentiment_data.csv')
+data['message'] = data['message'].astype('str')
+sentiments = vader.sentiment(data['message'].tolist())
+data['vader'] = [x[3] for x in sentiments]
+
+# counting = data['sentiment'].value_counts()
+
+data['difference'] = data['sentiment'] - data['vader']
+
+fig_per = px.line(data, x='tweetid', y='difference')
+fig_per.show()
+
+# fig = px.line(data, x='tweetid', y='vader')
+# fig.add_scatter(x=data['tweetid'], y=data['sentiment'], mode='lines')
+# fig.show()
--- main.py
+++ main.py
@@ -10,10 +10,5 @@ if __name__ == "__main__":
     print('Beginning Sentiment Analysis of Tweets...')
     print('Importing and Reading Dataset...')
     data = pd.read_csv('datasets/tweets.csv')
-    # I can't believe this doesn't work lmao
     print('Conducting Sentiment Analysis using VADER...')
-    sentiment = vader.sentiment(data['text'].tolist())
-    compound = [x[3] for x in sentiment]
-    date = data['created_at'].tolist()
     print('Plotting Sentiment towards Climate Change over Time')
-    plotting.plot_list(date, compound)
